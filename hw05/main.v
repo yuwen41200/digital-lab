55cbb7868b782be822d62a2aa8d1951a4d7b8b3d
yuwen41200 <yuwen41200@gmail.com>
2015-10-30 06:22:39 +0800

    Finished hw05, maybe. Not debugged yet.

--- hw05/main.v
+++ hw05/main.v
@@ -8,20 +8,20 @@ module main(
 	);
 
 localparam [1:0] S_IDLE = 2'b00, S_WAIT = 2'b01, S_SEND = 2'b10, S_INCR = 2'b11;
-localparam MEM_SIZE = 32;
+localparam MEM_SIZE = 256;
 
 wire btn_pressed;
 wire transmit;
-wire [7:0] tx_byte;
+wire [7:0]  tx_byte;
 wire received;
-wire [7:0] rx_byte;
+wire [7:0]  rx_byte;
 wire is_receiving;
 wire is_transmitting;
 wire recv_error;
-reg  [7:0] send_counter;
-reg  [1:0] current_state, next_state;
-reg  [7:0] data [0:MEM_SIZE-1];
-reg  [7:0] rx_temp;
+reg  [7:0]  send_counter, recv_counter;
+reg  [1:0]  current_state, next_state;
+reg  [7:0]  data [0:MEM_SIZE-1];
+reg  [15:0] recv_invalid;
 integer idx;
 
 debounce debounce(.clk(clk), .btn_input(btn), .btn_output(btn_pressed));
@@ -33,25 +33,36 @@ assign led = {7'b0, btn_pressed};
 assign tx_byte = data[send_counter];
 assign transmit = (current_state == S_WAIT) ? 1 : 0;
 
-always begin
-	data[ 0] = 8'h48; // H
-	data[ 1] = 8'h65; // e
-	data[ 2] = 8'h6C; // l
-	data[ 3] = 8'h6C; // l
-	data[ 4] = 8'h6F; // o
-	data[ 5] = 8'h2C; // ,
-	data[ 6] = 8'h20; // (space)
-	data[ 7] = 8'h57; // w
-	data[ 8] = 8'h6F; // o
-	data[ 9] = 8'h72; // r
-	data[10] = 8'h6C; // l
-	data[11] = 8'h64; // d
-	data[12] = 8'h21; // !
-	data[13] = 8'h20; // (space)
-	data[14] = 8'h0;  // (end of string)
-	for (idx = 15; idx < MEM_SIZE; idx = idx + 1)
-		data[idx] = 8'h0;
+// Circuit for Receiving Data
+
+always @(posedge clk) begin
+	if (rst) begin
+		recv_counter <= 0;
+		recv_invalid <= 0;
+	end
+	else if (received || is_receiving) begin
+		if (recv_invalid == 52083) begin // (1/9600 * 10) / (2 * 10^-8) = 52083.333
+			recv_counter <= recv_counter + 1;
+			recv_invalid <= 0;
+		end
+		else
+			recv_invalid <= recv_invalid + 1;
+	end
 end
+
+always @(posedge clk) begin
+	if (rst)
+		for (idx = 0; idx < MEM_SIZE; idx = idx + 1)
+			data[idx] <= 8'h0;
+	else if ((received || is_receiving) && (recv_invalid == 0)) begin
+		if (rx_byte >= 8'h61 && rx_byte <= 8'h7A)
+			data[recv_counter] <= rx_byte - 8'h20;
+		else
+			data[recv_counter] <= rx_byte;
+	end
+end
+
+// Circuit for Transmitting Data
 
 always @(posedge clk) begin
 	if (rst)
@@ -61,7 +72,7 @@ always @(posedge clk) begin
 end
 
 always @(posedge clk) begin
-	if (rst || (current_state == S_IDLE))
+	if (rst || current_state == S_IDLE)
 		send_counter <= 0;
 	else if (current_state == S_INCR)
 		send_counter <= send_counter + 1;
@@ -90,14 +101,6 @@ always @(*) begin
 			else
 				next_state = S_WAIT;
 	endcase
-end
-
-// The following logic stores the UART input in a temporary buffer
-always @(posedge clk) begin
-	if (rst)
-		rx_temp <= 8'b0;
-	else if (received)
-		rx_temp <= rx_byte;
 end
 
 endmodule
