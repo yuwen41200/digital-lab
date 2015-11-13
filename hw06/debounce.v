5fb7982c7e7de7bd1e49a3a7e3cd3eac83566ad3
yuwen41200 <yuwen41200@gmail.com>
2015-11-14 06:12:02 +0800

    Convertion not finished, but I want to eat breakfast and go to bed.

--- hw06/debounce.v
+++ hw06/debounce.v
@@ -1,31 +1,35 @@
-// Copyright Notice: This code is written by the TAs of this course.
+module debounce(
+		input clk,
+		input btn_in,
+		output reg btn_out
+	);
 
-module debounce(input clk, input btn_input, output btn_output);
-
-parameter DEBOUNCE_PERIOD = 1000000; /* 20 msec @ 50MHz */
+// 20 msec @ 50MHz
+parameter DEBOUNCE_PERIOD = 1000000;
 
 reg [20:0] counter;
-reg debounced_btn_state;
 reg pressed;
 
-assign btn_output = debounced_btn_state;
-
 always @(posedge clk) begin
-	if (btn_input == 0) begin
-		counter <= 0;
-		pressed <= 0;
-	end else begin
+	if (btn_in)
 		counter <= counter + 1;
-		pressed <= (debounced_btn_state == 1) ? 1 : 0;
-	end
+	else
+		counter <= 0;
 end
 
 always @(posedge clk) begin
-	// wait until the button is pressed continuously for 20 msec
-	if (counter == DEBOUNCE_PERIOD && pressed == 0)
-		debounced_btn_state <= 1;
-	else
-		debounced_btn_state <= (btn_input == 0) ? 0 : btn_output;
+	if (counter >= DEBOUNCE_PERIOD && pressed) begin
+		btn_out <= 0;
+		pressed <= 1;
+	end
+	else if (counter >= DEBOUNCE_PERIOD && ~pressed) begin
+		btn_out <= 1;
+		pressed <= 1;
+	end
+	else if (~btn_in) begin
+		btn_out <= 0;
+		pressed <= 0;
+	end
 end
 
 endmodule
--- hw06/main.v
+++ hw06/main.v
@@ -7,33 +7,42 @@ module main(
 		output [7:0] led
 	);
 
-localparam [1:0] S_IDLE = 2'b00, S_WAIT = 2'b01, S_SEND = 2'b10, S_INCR = 2'b11;
-localparam MEM_SIZE = 256;
+localparam [2:0] S1_IDLE = 0, S1_ROW0 = 1, S1_ROW1 = 2, S1_ROW2 = 3, S1_ROW3 = 4, S1_PEND = 5, S1_DONE = 6;
+localparam [1:0] S2_IDLE = 0, S2_WAIT = 1, S2_SEND = 2, S2_INCR = 3;
 
-wire btn_pressed;
+wire btn_r;
 wire transmit;
-wire [7:0]  tx_byte;
+wire [7:0] tx_byte;
 wire received;
-wire [7:0]  rx_byte;
+wire [7:0] rx_byte;
 wire is_receiving;
 wire is_transmitting;
 wire recv_error;
-reg  [7:0]  send_counter, recv_counter;
-reg  [1:0]  current_state, next_state;
-reg  [7:0]  data [0:MEM_SIZE-1];
-reg  same_btn_press;
+wire multi_done;
+
+reg [7:0]  mtx_a [0:15];
+reg [7:0]  mtx_b [0:15];
+reg [17:0] mtx_c [0:15];
+reg [17:0] temp  [0:15];
+reg [2:0] curr_state1, next_state1;
+reg [1:0] curr_state2, next_state2;
+reg [7:0] recv_counter, send_counter;
+reg [7:0] result [0:255];
 integer idx;
 
-debounce debounce(.clk(clk), .btn_input(btn), .btn_output(btn_pressed));
+debounce debounce(.clk(clk), .btn_in(btn), .btn_out(btn_r));
 
 uart uart(.clk(clk), .rst(rst), .rx(rx), .tx(tx), .transmit(transmit), .tx_byte(tx_byte), .received(received),
 	.rx_byte(rx_byte), .is_receiving(is_receiving), .is_transmitting(is_transmitting), .recv_error(recv_error));
 
-assign led = {7'b0, btn_pressed};
-assign tx_byte = data[send_counter];
-assign transmit = (current_state == S_WAIT) ? 1 : 0;
+assign led = {7'b0, recv_error};
+assign tx_byte = result[send_counter];
+assign multi_done = (curr_state1 == S1_DONE) ? 1 : 0;
+assign transmit = (curr_state2 == S2_WAIT) ? 1 : 0;
 
-// Circuit for Receiving Data
+/**
+ * receive two matrices from a binary file
+ */
 
 always @(posedge clk) begin
 	if (rst)
@@ -43,64 +52,112 @@ always @(posedge clk) begin
 end
 
 always @(posedge clk) begin
-	if (rst)
-		for (idx = 0; idx < MEM_SIZE; idx = idx + 1)
-			data[idx] <= 8'h0;
+	if (rst) begin
+		for (idx = 0; idx < 16; idx = idx + 1)
+			mtx_a[idx] <= 8'h0;
+		for (idx = 0; idx < 16; idx = idx + 1)
+			mtx_b[idx] <= 8'h0;
+	end
 	else if (received) begin
-		if (rx_byte >= 8'h61 && rx_byte <= 8'h7A)
-			data[recv_counter] <= rx_byte - 8'h20;
-		else
-			data[recv_counter] <= rx_byte;
+		if (recv_counter < 16)
+			mtx_a[recv_counter] <= rx_byte;
+		else if (recv_counter >= 16 && recv_counter < 32)
+			mtx_b[recv_counter-16] <= rx_byte;
 	end
 end
 
-// Circuit for Transmitting Data
+/**
+ * multiply the received matrices and convert the results to text
+ */
 
 always @(posedge clk) begin
 	if (rst)
-		current_state <= S_IDLE;
+		curr_state1 <= S1_IDLE;
 	else
-		current_state <= next_state;
+		curr_state1 <= next_state1;
 end
 
 always @(posedge clk) begin
-	if (rst || current_state == S_IDLE)
-		send_counter <= 0;
-	else if (current_state == S_INCR)
-		send_counter <= send_counter + 1;
+	if (curr_state1 >= 1 && curr_state1 <= 4) begin
+		for (idx = 0; idx < 4; idx = idx + 1) begin
+			temp[idx+0]  <= mtx_a[curr_state1*4-4] * mtx_b[idx+0];
+			temp[idx+4]  <= mtx_a[curr_state1*4-3] * mtx_b[idx+4];
+			temp[idx+8]  <= mtx_a[curr_state1*4-2] * mtx_b[idx+8];
+			temp[idx+12] <= mtx_a[curr_state1*4-1] * mtx_b[idx+12];
+		end
+	end
 end
 
 always @(posedge clk) begin
-	if (rst)
-		same_btn_press <= 0;
-	else if (btn_pressed && ~same_btn_press)
-		same_btn_press <= 1;
-	else if (~btn_pressed)
-		same_btn_press <= 0;
+	if (curr_state1 >= 2 && curr_state1 <= 5) begin
+		for (idx = 0; idx < 4; idx = idx + 1) begin
+			mtx_c[curr_state1*4+idx-8]  <= temp[idx+0] + temp[idx+4] + temp[idx+8] + temp[idx+12];
+		end
+	end
 end
 
 always @(*) begin
-	case (current_state)
-		S_IDLE: // wait for a button click
-			if (btn_pressed && ~same_btn_press)
-				next_state = S_WAIT;
+	case (curr_state1)
+		S1_IDLE:
+			if (btn_r)
+				next_state1 = S1_ROW0;
 			else
-				next_state = S_IDLE;
-		S_WAIT: // wait for the transmission of the current data byte begins
+				next_state1 = S1_IDLE;
+		S1_ROW0:
+			next_state1 = S1_ROW1;
+		S1_ROW1:
+			next_state1 = S1_ROW2;
+		S1_ROW2:
+			next_state1 = S1_ROW3;
+		S1_ROW3:
+			next_state1 = S1_PEND;
+		S1_PEND:
+			next_state1 = S1_DONE;
+		S1_DONE:
+			next_state1 = S1_IDLE;
+	endcase
+end
+
+/**
+ * output the resulting text to a terminal
+ */
+
+always @(posedge clk) begin
+	if (rst)
+		curr_state2 <= S2_IDLE;
+	else
+		curr_state2 <= next_state2;
+end
+
+always @(posedge clk) begin
+	if (rst || curr_state2 == S2_IDLE)
+		send_counter <= 0;
+	else if (curr_state2 == S2_INCR)
+		send_counter <= send_counter + 1;
+end
+
+always @(*) begin
+	case (curr_state2)
+		S2_IDLE:
+			if (multi_done)
+				next_state2 = S2_WAIT;
+			else
+				next_state2 = S2_IDLE;
+		S2_WAIT:
 			if (is_transmitting)
-				next_state = S_SEND;
+				next_state2 = S2_SEND;
 			else
-				next_state = S_WAIT;
-		S_SEND: // wait for the transmission of the current data byte finishes
+				next_state2 = S2_WAIT;
+		S2_SEND:
 			if (is_transmitting)
-				next_state = S_SEND;
+				next_state2 = S2_SEND;
 			else
-				next_state = S_INCR;
-		S_INCR: // transmit the next data byte, i.e. next character
+				next_state2 = S2_INCR;
+		S2_INCR:
 			if (tx_byte == 8'h0)
-				next_state = S_IDLE;
+				next_state2 = S2_IDLE;
 			else
-				next_state = S_WAIT;
+				next_state2 = S2_WAIT;
 	endcase
 end
 
