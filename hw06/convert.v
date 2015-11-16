0d1d7176a91e8d7923435e21ba6a05258f7434b6
yuwen41200 <yuwen41200@gmail.com>
2015-11-16 23:05:06 +0800

    Bug fixes. Finished! Congratulations!!

--- hw06/convert.v
+++ hw06/convert.v
@@ -7,10 +7,10 @@ module convert(
 		output [7:0] text_out4
 	);
 
-assign text_out4 = (binary_in[3:0]   < 10) ? (binary_in[3:0]   + 8'd48) : (binary_in[3:0]   + 8'd65);
-assign text_out3 = (binary_in[7:4]   < 10) ? (binary_in[7:4]   + 8'd48) : (binary_in[7:4]   + 8'd65);
-assign text_out2 = (binary_in[11:8]  < 10) ? (binary_in[11:8]  + 8'd48) : (binary_in[11:8]  + 8'd65);
-assign text_out1 = (binary_in[15:12] < 10) ? (binary_in[15:12] + 8'd48) : (binary_in[15:12] + 8'd65);
+assign text_out4 = (binary_in[3:0]   < 10) ? (binary_in[3:0]   + 8'd48) : (binary_in[3:0]   + 8'd55);
+assign text_out3 = (binary_in[7:4]   < 10) ? (binary_in[7:4]   + 8'd48) : (binary_in[7:4]   + 8'd55);
+assign text_out2 = (binary_in[11:8]  < 10) ? (binary_in[11:8]  + 8'd48) : (binary_in[11:8]  + 8'd55);
+assign text_out1 = (binary_in[15:12] < 10) ? (binary_in[15:12] + 8'd48) : (binary_in[15:12] + 8'd55);
 assign text_out0 =  binary_in[17:16] + 8'd48;
 
 endmodule
